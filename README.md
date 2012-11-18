0fd75698c21fb38bcd3248ff0bec916a395bd9a1
Seth B. Altshuler <sethb78@gmail.com>
2012-11-18 12:13:55 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+My attempt at creating my own website for the College Bowl Pool.
+
+First, I have created the Rails app, added Rspec, Guard, and Spork, and setup the Repository and made the initial push.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BowlPool::Application.config.secret_token = 'ecb445706f98b1b2c7258ea04844daaf39267214cb6e8444fe174219a4feb28011cfda372e023eb96cab4111e259dffb5fbf3f45d02b7fc5e080afa5bf0908f1'
